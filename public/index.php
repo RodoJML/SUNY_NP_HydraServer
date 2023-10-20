cb7629b26eccf322017a13d67ae789ba6e82a181
Rodolfo <rodojml@me.com>
2023-10-20 16:04:33 -0400

    Setting up folder structure

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SUNY New Paltz | Hydra Lab - Computer Science</title>
+    
+    <link rel="index.css" href="/assets/index.css">
+
+</head>
+
+<body>
+    <h1>TEST</h1>
+    Lorep Ipsum
+</body>
+
+</html>
