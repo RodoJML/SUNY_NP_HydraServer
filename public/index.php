eb70aef28ec1608a21c2d9a8ccfef10923586eb8
Rodolfo <rodojml@me.com>
2023-10-20 21:52:43 -0400

    testing CSS

--- public/index.php
+++ public/index.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SUNY New Paltz | Hydra Lab - Computer Science</title>
-    
-    <link rel="index.css" href="/assets/index.css">
-
+    <link rel="stylesheet" href="../assets/index.css">
 </head>
 
 <body>
