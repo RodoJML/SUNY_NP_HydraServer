627c12c9aa1aca6b9e6652d84ab3547e32b3d175
Rodolfo <rodojml@me.com>
2023-10-20 23:44:03 -0400

    Adding logo and improving folder structure

---
+++ client/src/assets/components/navbar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NavBar</title>
+</head>
+<body>
+
+    <ul class="topnav">
+        <li><a>Home</a></li>
+        <li><a>Server Request</a></li>
+        <li><a>About</a></li>
+        <li><a>Calendar<a></li>
+        <li><a>Lab Rules<a></li>
+        <li><a>Clubs<a></li>
+        <li><a>Resources<a></li>
+    </ul>
+    
+</body>
+</html>
---
+++ client/src/assets/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SUNY New Paltz | Hydra Lab - Computer Science</title>
+    
+    <!-- Loading CSS  -->
+    <link rel="stylesheet" href="../assets/css/index.css">
+</head>
+
+<body>
+    <div>
+
+        <div class="Logo">
+            <img class="HydraLabLogo" src="../assets/images/HydraLabLogo.png" alt="HydraLabLogo">
+        </div>
+
+        
+
+    </div>
+    
+</body>
+
+</html>
