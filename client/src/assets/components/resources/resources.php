296f79394699739b7318ad017abe12ddb5d21c5f
Rodolfo <rodojml@me.com>
2023-10-29 18:20:14 -0400

    Beginning with resources page

---
+++ client/src/assets/components/resources/resources.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/resources/resources.css">
+    <title>Resources</title>
+</head>
+<body>
+    <div class="resources">
+
+        <div class="heading">
+            <h1>Sources</h1>
+        </div>
+
+        <p>Here are some useful developer resources</p>
+
+        <div class="subheading">
+            <h1>Java:</h1>
+
+            <div class="grid-container">
+                
+                <div class="grid-item">
+                    <img class="image" src="https://www.w3schools.com/java/pic_java.jpg" alt="Java">
+                    <h4 class="title">W3 Schools Java Tutorial</h4>
+                    <a class="description" href="https://www.w3schools.com/java/">
+                        Java tutorials that covers the basics of Java
+                    </a>
+                </div>
+
+                <div class="grid-item">
+                    test
+                </div>
+
+                <div class="grid-item">
+                    test
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
