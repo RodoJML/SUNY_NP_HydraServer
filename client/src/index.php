7ff6ca9c75384600cea2dd9a028b49af7457b246
Rodolfo <rodojml@me.com>
2023-10-29 17:34:05 -0400

    Stylesheets structure improved for each component

--- client/src/index.php
+++ client/src/index.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/index/index.css">
-    <link rel="stylesheet" href="./assets/css/rules/rules.css">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    
     <title> ⚛️ Hydra Lab | CS SUNY NP</title>
 </head>
 
@@ -15,13 +12,10 @@
     <div class="bodyContainer">
         <?php include './assets/components/template/header.php'; ?>
         <?php include './assets/components/template/navbar.php'; ?>
-
         <div class="displayArea" style="display: none"></div>
         <img class="indexMainPic" src="./assets/images/hydralabwithtext.png" alt="indexMainPic">
-
         <?php include './assets/components/index/labhours.php'; ?>
         <?php include './assets/components/index/announcements.php'; ?>
-
         <?php include './assets/components/template/footer.php'; ?>
     </div>
 
