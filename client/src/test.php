dc168035a50bc6def021f81f769449fc82a6a5c6
Rodolfo <rodojml@me.com>
2023-10-30 23:55:00 -0400

    Removing test files for serverrequest animation

--- client/src/test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-            $studentid = $_POST["studentid"];
-            $name = $_POST["name"];
-            $email = $_POST["email"];
-            echo "<p>Student ID: $studentid</p>";
-            echo "<p>Name: $name</p>";
-            echo "<p>Email: $email</p>";
-?>
