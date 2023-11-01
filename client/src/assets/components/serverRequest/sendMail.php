84fd174553f55c61755c243431129102602f7ee8
Rodolfo <rodojml@me.com>
2023-11-01 14:25:37 -0400

    Send email adding confirmation

--- client/src/assets/components/serverRequest/sendMail.php
+++ client/src/assets/components/serverRequest/sendMail.php
@@ -1,6 +1,6 @@
 <?php
     header("Content-Type: application/json");
     mail("menesesr1@newpaltz.edu", "Server Access Request (".$_GET["id"].")", $_GET["message"]);
-    mail($_GET["emailAdd"], "Hydra Server Request Confirmation", "Your request has been sent to the Hydra Lab administrators.\nTry to access the server again in the next days.\n\n\nThis request has been sent from https://hydra.newpaltz.edu/");
+    mail($_GET["emailAdd"], "Hydra Server Request Confirmation", "Your request has been sent to the Hydra Lab administrators.\nTry to access the server again in the next days.\n\n\nThis request was sent on: ".date("Y-m-d H:i:s")." from https://hydra.newpaltz.edu/");
     echo json_encode(["response" => "Email sent using PHP"]);
 ?>
