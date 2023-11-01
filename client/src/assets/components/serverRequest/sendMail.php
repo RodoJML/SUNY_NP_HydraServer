f1e6f6270ab164104d6394dea6667b1d5335ad34
Rodolfo <rodojml@me.com>
2023-10-31 23:25:22 -0400

    Send server request access fully implemented

--- client/src/assets/components/serverRequest/sendMail.php
+++ client/src/assets/components/serverRequest/sendMail.php
@@ -1,5 +1,5 @@
 <?php
     header("Content-Type: application/json");
-    mail("menesesr1@newpaltz.edu", "Server Access Request (".$_GET["id"].")", $_GET["message"]);
+    mail("hoffmank4@newpaltz.edu", "Server Access Request (".$_GET["id"].")", $_GET["message"]);
     echo json_encode(["response" => "Email sent"]);
 ?>
