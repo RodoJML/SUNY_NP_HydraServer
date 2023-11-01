a706d1c6ab36fb4a25307aa9faf127a8bf6bf3f7
Rodolfo <rodojml@me.com>
2023-10-31 23:42:23 -0400

    testing .env

--- client/src/assets/components/serverRequest/sendMail.php
+++ client/src/assets/components/serverRequest/sendMail.php
@@ -1,5 +1,9 @@
 <?php
+    require_once realpath(__DIR__ . '/.env');
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
     header("Content-Type: application/json");
-    mail("menesesr1@newpaltz.edu", "Server Access Request (".$_GET["id"].")", $_GET["message"]);
-    echo json_encode(["response" => "Email sent"]);
+    // mail("menesesr1@newpaltz.edu", "Server Access Request (".$_GET["id"].")", $_GET["message"]);
+    echo json_encode(["response" => $_ENV['EMAIL']]);
 ?>
