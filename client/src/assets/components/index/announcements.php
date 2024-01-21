e1faf82456e8b8016457487d0a971b30cb26a91f
Rodolfo <rodojml@me.com>
2024-01-21 18:49:19 -0500

    Styling updated

--- client/src/assets/components/index/announcements.php
+++ client/src/assets/components/index/announcements.php
@@ -19,7 +19,7 @@
             </div>
             
             <div class="grid-item2">
-                <h1 class="title"> Example Announcement</h1>
+                <h1 class="title">CS @ SUNY NP</h1>
                 <p class="text">With a computer science degree from SUNY New Paltz, you will learn all of the fundamentals needed to be a future self-learner. With small class sizes, personal attention from renowned scholars and collaborations with industry, every computer science student has the opportunity to work with current technology in an environment where project work and skills-development are emphasized.</p>
             </div>
         </div>
