e5ae473084efa0be373cc221d99e60ebfd65dfcc
Rodolfo <rodojml@me.com>
2024-01-22 16:43:41 -0500

    All colors updated from purple to dark green

--- client/src/index.php
+++ client/src/index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./assets/css/index/index.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <title>🐉 Hydra Lab | CS SUNY NP</title>
+        <title>🐉 Hydra Lab | CS SUNY New Paltz</title>
     </head>
 
     <body>    
